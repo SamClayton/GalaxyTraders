01b7bfc02013dbcde95c48f040c276fbed3a6282
Sam Clayton <sam.clayton@gmail.com>
2016-06-25 22:52:27 -0500

    Added Flow router with some defs and some styling

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
+import './templates/app/main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
