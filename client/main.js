1a8b883aad1f10e82179742bee75f8a4100c7279
Sam Clayton <sam.clayton@gmail.com>
2016-09-18 19:57:10 -0700

    Some basic sAlert configuration; prospective package comments

--- client/main.js
+++ client/main.js
@@ -1,5 +1,39 @@
 import { Template } from 'meteor/templating';
 
+Meteor.startup(function() {
+    Meteor.subscribe('Games');
+    Meteor.subscribe('Rankings');
+
+    sAlert.config({
+        effect: 'jelly',
+        position: 'top-right',
+        timeout: 5000,
+        html: false,
+        onRouteClose: true,
+        stack: true,
+        // or you can pass an object:
+        // stack: {
+        //     spacing: 10 // in px
+        //     limit: 3 // when fourth alert appears all previous ones are cleared
+        // }
+        offset: 20, // in px - will be added to first alert (bottom or top - depends of the position in config)
+        beep: false,
+        // examples:
+        // beep: '/beep.mp3'  // or you can pass an object:
+        // beep: {
+        //     info: '/beep-info.mp3',
+        //     error: '/beep-error.mp3',
+        //     success: '/beep-success.mp3',
+        //     warning: '/beep-warning.mp3'
+        // }
+        onClose: _.noop //
+        // examples:
+        // onClose: function() {
+        //     /* Code here will be executed once the alert closes. */
+        // }
+    });
+});
+
 // Make the login popup stay within bounds of the page
 Template.nav.events({
   'click .dropdown-toggle'(event, instance) {
@@ -8,5 +42,3 @@ Template.nav.events({
 });
 
 
-Meteor.subscribe('Games');
-Meteor.subscribe('Rankings');
